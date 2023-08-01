c23cc56fe24d156cb1924bf78ce15843439dcd0f
Itay Shklyar <itay.shklyar@gmail.com>
2023-08-01 16:48:25 +0300

    basic rules before starting programing

---
+++ main.py
@@ -0,0 +1,14 @@
+# battleship game contains ROW(A-J)  and COLUMN (1-10) = 10X10 grid. there are two boards of ROW and COLUMN,
+# lower to arrange self ships, and upper to defeat enemy's ships. each gets 5 ships containing: 1 ship of 2 dots + 2
+# ships of 3 dots + 1 ship of 4 dots + 1 ship of 5 dots. no diagonal or overlapping ships when placing on grid.
+# player 1 begins using letter and number. example: A1 if there is no ship player2 says miss. then player1 puts '-'
+# on there. else 'S' as strike and player2 places X on his ship. ship on terminal grid could look like this [....] or
+# [..] dots as the length of the ship and if hit then a ship of 3 dots such as [...] will look like [..X] and so on [
+# .XX] depends on where is has been hit
+# if the ship has eliminated, the opponent says "you sunk my ship"
+
+
+
+
+
+
